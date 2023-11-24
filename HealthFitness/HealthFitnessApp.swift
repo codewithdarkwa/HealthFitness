58390ed3c6f759757084462e68b315bd860ed3c1
codewithdarkwa <jnrdrkw1@gmail.com>
2023-11-24 13:50:22 +0000

    Initial Commit

---
+++ HealthFitness/HealthFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthFitnessApp.swift
+//  HealthFitness
+//
+//  Created by Darkwa John on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
